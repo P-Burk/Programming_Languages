f515001648bcd1ee75ba1e0eceee60457e6b1fce
P-Burk <P-Burk@users.noreply.github.com>
2021-03-16 23:17:44 -0400

    added functions for displays, time calc, menu loop

--- Project_1/Clock_main.cpp
+++ Project_1/Clock_main.cpp
@@ -7,54 +7,92 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <ctime>        // needed for srand(time(0))
 using namespace std;
 
-// function for displaying the top and bottom lines of asterisks
-void AstriskLine() {
-    cout << setw(26) << setfill('*') << "*" << "    " << setw(26) << setfill('*') << "*" << endl;
+
+// outputs the time display. Takes hours, minutes, and seconds as arguements
+void TimeDisplay (int hours, int minutes, int seconds) {
+    cout << setw(26) << setfill('*') << "*" << "    " << setw(26) << setfill('*') << "*" << endl;           // asterisk line
+    printf("*      %s     *", "12-Hour Clock"), cout << "    ";                                             // 12-hr clock header
+    printf("*      %s     *", "24-Hour Clock"), cout << endl;                                               // 24-hr clock header
+
+    // if/else branch based on whether hours is divisible by 12 to determine AM or PM for 12-hr clock
+    if (hours / 12 != 0) {      
+        printf("*      %02d:%02d:%02d %s      *", (hours - 12), minutes, seconds, "P M"), cout << "    ";   // subtract 12 from hours
+    }                                                                                                       // to get PM time
+    else {
+        printf("*      %02d:%02d:%02d %s      *", hours, minutes, seconds, "A M"), cout << "    ";          // 12-hr AM time
+    }
+
+    printf("*        %02d:%02d:%02d        *", hours, minutes, seconds), cout << endl;                      // 24-hr clock 
+    cout << setw(26) << setfill('*') << "*" << "    " << setw(26) << setfill('*') << "*" << endl;           // asterisk line
 }
 
-// function for displaying the 12-hr and 24-hr clock headers
-void TimeHeaders() {
-    printf("*      %s     *", "12-Hour Clock");
-    cout << "    ";
-    printf("*      %s     *", "24-Hour Clock");
-    cout << endl;
+// outputs the menu display
+void MenuDisplay() {
+    cout << setw(26) << setfill('*') << "*" << endl;                // asterisk line
+    printf("*  %s      *", "1 - Add One Hour"), cout << endl;       // option 1 line
+    printf("*  %s    *", "2 - Add One Minute"), cout << endl;       // option 2 line
+    printf("*  %s    *", "3 - Add One Second"), cout << endl;       // option 3 line
+    printf("*  %s      *", "4 - Exit Program"), cout << endl;       // option 4 line
+    cout << setw(26) << setfill('*') << "*" << endl;                // asterisk line
 }
 
-// function for displaying the 12-hr AM time
-void AMTimeFormat(int hours, int min, int sec) {
-    printf("*      %02d:%02d:%02d %s      *", hours, min, sec, "A M");
-    cout << "    ";
-}
-
-// function for displaying the 12-hr PM time
-void PMTimeFormat(int hours, int min, int sec) {
-    printf("*      %02d:%02d:%02d %s      *", hours, min, sec, "P M");
-    cout << "    ";
-}
-
-// function for displaying the 24-hr time
-void TFHTimeFormat(int hours, int min, int sec) {
-    printf("*      %02d:%02d:%02d %s      *", hours, min, sec, "P M");
-    cout << endl;
+// calculates hrs, min, and sec. 
+// takes total seconds in and passes hrs/min/sec out by reference 
+// TODO: find way to roll over seconds, minutes, and hrs
+void TimeCalc(int secondsIn, int &hrsOut, int &minOut, int &secOut) {
+    hrsOut = (secondsIn / 60) / 60;
+    minOut = (secondsIn / 60) - (hrsOut * 60);
+    secOut = secondsIn % 60;
 }
 
 int main() {
     // defines and initializes variables with default values
-    int thHours = 00;
-    int thMinutes = 00;
-    int thSeconds = 00;
-    int tfhHours = 00;
-    int tfhMinutes = 00;
-    int tfhSeconds = 00;
+    int totalHours = 0;
+    int totalMinutes = 0;
+    int totalSeconds = 0;
+    int userSelection = 0;
+    int startNumber;
 
-    AstriskLine();
-    TimeHeaders();
-    PMTimeFormat(thHours, thMinutes, thSeconds);
-    TFHTimeFormat(tfhHours, tfhMinutes, tfhSeconds);
-    AstriskLine();
-    
+    // generates random number to be used as seconds
+    //srand(time(0));
+    //cout << (startNumber = (rand() % 86400)) << endl;
+
+    //FIXME: delete this before turning in. using as placeholder to force times for testing
+    startNumber = 50000;
+    cout << "Starting number is: " << startNumber << endl;
+
+    TimeCalc(startNumber, totalHours, totalMinutes, totalSeconds);
+    TimeDisplay(totalHours, totalMinutes, totalSeconds);
+    MenuDisplay();
+
+    while (userSelection != 4) {
+        cin >> userSelection;
+
+        switch (userSelection) {
+        case 1:                         // adds one hour
+            startNumber += 3600;
+            break;
+        case 2:                         // adds one minute
+            startNumber += 60;
+            break;
+        case 3:                         // adds one second
+            startNumber++;
+            break;
+        case 4:                         // exits program
+            cout << "Goodbye" << endl;
+            return 0;
+        default:
+            break;
+        }
+
+    TimeCalc(startNumber, totalHours, totalMinutes, totalSeconds);
+    TimeDisplay(totalHours, totalMinutes, totalSeconds);
+    MenuDisplay();
+
+    }
 
     return 0;
 }
