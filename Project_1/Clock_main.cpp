dabb657d49c75bc6e9192c029bca55b9f4563d96
P-Burk <P-Burk@users.noreply.github.com>
2021-03-17 23:05:26 -0400

    fixed rollover, 12pm time display, current time

--- Project_1/Clock_main.cpp
+++ Project_1/Clock_main.cpp
@@ -10,6 +10,9 @@
 #include <ctime>        // needed for srand(time(0))
 using namespace std;
 
+// declares and initializes global constant variable for total seconds in 1 day
+const int secondsInOneDay = 86400;
+
 
 // outputs the time display. Takes hours, minutes, and seconds as arguements
 void TimeDisplay (int hours, int minutes, int seconds) {
@@ -18,9 +21,12 @@ void TimeDisplay (int hours, int minutes, int seconds) {
     printf("*      %s     *", "24-Hour Clock"), cout << endl;                                               // 24-hr clock header
 
     // if/else branch based on whether hours is divisible by 12 to determine AM or PM for 12-hr clock
-    if (hours / 12 != 0) {      
-        printf("*      %02d:%02d:%02d %s      *", (hours - 12), minutes, seconds, "P M"), cout << "    ";   // subtract 12 from hours
-    }                                                                                                       // to get PM time
+    if (hours == 12) {
+        printf("*      %02d:%02d:%02d %s      *", hours, minutes, seconds, "P M"), cout << "    ";          // handles PM time when hours is exactly 12
+    } 
+    else if (hours / 12 != 0) {      
+        printf("*      %02d:%02d:%02d %s      *", (hours - 12), minutes, seconds, "P M"), cout << "    ";   // subtract 12 from hours to get PM time
+    }                                                                                                      
     else {
         printf("*      %02d:%02d:%02d %s      *", hours, minutes, seconds, "A M"), cout << "    ";          // 12-hr AM time
     }
@@ -41,13 +47,24 @@ void MenuDisplay() {
 
 // calculates hrs, min, and sec. 
 // takes total seconds in and passes hrs/min/sec out by reference 
-// TODO: find way to roll over seconds, minutes, and hrs
-void TimeCalc(int secondsIn, int &hrsOut, int &minOut, int &secOut) {
-    hrsOut = (secondsIn / 60) / 60;
-    minOut = (secondsIn / 60) - (hrsOut * 60);
-    secOut = secondsIn % 60;
+void TimeCalc(int& secondsInOut, int &hrsOut, int &minOut, int &secOut) {
+    // if and nested if/else statements handle the rollover of the clock for times > 23:59:59
+    if (secondsInOut >= secondsInOneDay) {
+        int secondsHolder = secondsInOut - secondsInOneDay;     // secondsHolder gets assigned the seconds in excess of secondsInOneDay to handle rollovers > 1 second
+        if (secondsHolder != 0) {                               // if seconds holder isn't 0, assign that number to secondsInOut to get used in the time calculation
+            secondsInOut = secondsHolder;  
+        }
+        else {                                                  // else statement handls single second rollover
+            secondsInOut = 0;
+        }
+    }
+    // calculates the hours. minutes, and seconds based off of secondsInOut
+    hrsOut = (secondsInOut / 60) / 60;
+    minOut = (secondsInOut / 60) - (hrsOut * 60);
+    secOut = secondsInOut % 60;
 }
 
+// main driver code of program
 int main() {
     // defines and initializes variables with default values
     int totalHours = 0;
@@ -56,21 +73,19 @@ int main() {
     int userSelection = 0;
     int startNumber;
 
-    // generates random number to be used as seconds
-    //srand(time(0));
-    //cout << (startNumber = (rand() % 86400)) << endl;
+    // gets current time EST via finding the remainder of (epoch seconds - 4hrs) / total seconds in a day
+    startNumber = (time(0) - 14400) % secondsInOneDay;
 
-    //FIXME: delete this before turning in. using as placeholder to force times for testing
-    startNumber = 50000;
-    cout << "Starting number is: " << startNumber << endl;
-
+    // calculate time and output both displays
     TimeCalc(startNumber, totalHours, totalMinutes, totalSeconds);
     TimeDisplay(totalHours, totalMinutes, totalSeconds);
     MenuDisplay();
 
+    // while loop to continuously loop through program until user selects "4"
     while (userSelection != 4) {
         cin >> userSelection;
 
+        // switch statement to handle option choice branches
         switch (userSelection) {
             case 1:                         // adds one hour
                 startNumber += 3600;
@@ -88,11 +103,10 @@ int main() {
                 break;
         }
 
+    // once user chooses option, calculate time with new startNumber and output both displays
     TimeCalc(startNumber, totalHours, totalMinutes, totalSeconds);
     TimeDisplay(totalHours, totalMinutes, totalSeconds);
     MenuDisplay();
-
     }
-
     return 0;
 }
